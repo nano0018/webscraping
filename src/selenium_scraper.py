96948a9dea66df6ae0f834365d7eea4bd594a74b
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-02-24 19:25:54 -0500

    Se agrega script para pruebas

--- src/selenium_scraper.py
+++ src/selenium_scraper.py
@@ -17,7 +17,7 @@ def web_scraper(url, language="es"):
     # Browser options
     options = webdriver.ChromeOptions()
 
-    #Set the translate language
+    # Set the translate language
     options.add_argument("--lang=" + language)
     prefs = {
         "translate_whitelists": {"en-US": "hi"},
@@ -38,34 +38,70 @@ def web_scraper(url, language="es"):
     action_chains = ActionChains(driver)
     action_chains.context_click().perform()
 
-    # Force to use google translate
-    for i in range(3):
-        pyautogui.sleep(0.25)
-        pyautogui.press('up')
-    pyautogui.press('enter')
+    def scroll_page():
+        """Scroll the page and force to use google translate"""
 
-    time.sleep(2)
-    for i in range(3, 0, -1):
-        place = "window.scrollTo({ top: (document.body.scrollHeight / " + \
-            str(i) + "), behavior: 'smooth' });"
-        driver.execute_script(place)
-        time.sleep(0.5)
-    time.sleep(2)
-    print('Done!')
+        pyautogui.press('esc')
+        pyautogui.moveTo(100, 200, 2)
+        pyautogui.click(button='right')
+        for i in range(3):
+            pyautogui.sleep(0.25)
+            pyautogui.press('up')
+        pyautogui.press('enter')
+        time.sleep(2)
+        pyautogui.press('esc')
 
-    # Define new set to gater all non-repeating href links
+        for i in range(3, 0, -1):
+            place = "window.scrollTo({ top: (document.body.scrollHeight / " + \
+                str(i) + "), behavior: 'smooth' });"
+            driver.execute_script(place)
+            time.sleep(0.5)
+        time.sleep(2)
+        print('Done!')
+
+    def save_page(path="", filename=""):
+        full_path = "//web//"
+        if not path:
+            full_path = "//web//" + path + "//"
+        full_path += filename
+        html_source_code = driver.page_source
+        text_file = open((current_path + full_path),
+                         "w", encoding="utf-8")
+        text_file.write(html_source_code)
+        text_file.close()
+
+    # Scrolling down for full image load
+    scroll_page()
+
+    # Create index.html
+    save_page(filename="index.html")
+
+    # Define new set to gather all non-repeating href links
     href_links = set()
     a_list = driver.find_elements_by_tag_name("a")
     for link in a_list:
         href_links.add(link.get_attribute("href"))
-    print('Found:',len(href_links), 'link(s)')
+    print('Found:', len(href_links), 'link(s)')
 
-    # Create index.html
-    html_source_code = driver.page_source
-    text_file = open((current_path + "//web//index.html"),
-                     "w", encoding="utf-8")
-    text_file.write(html_source_code)
-    text_file.close()
+    for href_link in href_links:
+
+        raw_url = url.replace('https://', '')
+        if not href_link:
+            continue
+
+        href_data = href_link.split('/')
+        index_path = len(href_data) - 1
+
+        if not href_data[index_path]:
+            index_path -= 1
+
+        new_url = href_data[index_path]
+        if new_url == raw_url:
+            continue
+
+        driver.get(url + "/" + new_url)
+        scroll_page()
+        save_page(path=new_url, filename=(new_url + ".html"))
 
 
 web_scraper('https://www.classcentral.com/', "hi")
