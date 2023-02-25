59e9ba319b800bf33acbb0baf2dfef9465051184
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-02-25 15:14:17 -0500

    Se modifica script de listado
    Se arregla error con el live site

--- src/sorted_list.py
+++ src/sorted_list.py
@@ -5,8 +5,8 @@ Returns sorted list of elements in the website
 import glob
 import os
 
-def create_html_index():
 
+def create_html_index():
     """
     Creates an index.html with all scrapped html
     """
@@ -16,7 +16,7 @@ def create_html_index():
 
     # be printed simultaneously.
     print("\nPrinting all html files")
-    dummy_html =  open(cur_path + '\\index.html', 'w', encoding='utf-8')
+    dummy_html = open(cur_path + '\\index.html', 'w', encoding='utf-8')
     dummy_html.write('<!DOCTYPE html>')
     dummy_html.write('\n')
     dummy_html.write('<html lang="en">')
@@ -25,9 +25,11 @@ def create_html_index():
     dummy_html.write('\n')
     dummy_html.write('    <meta charset="UTF-8">')
     dummy_html.write('\n')
-    dummy_html.write('    <meta http-equiv="X-UA-Compatible" content="IE=edge">')
+    dummy_html.write(
+        '    <meta http-equiv="X-UA-Compatible" content="IE=edge">')
     dummy_html.write('\n')
-    dummy_html.write('    <meta name="viewport" content="width=device-width, initial-scale=1.0">')
+    dummy_html.write(
+        '    <meta name="viewport" content="width=device-width, initial-scale=1.0">')
     dummy_html.write('\n')
     dummy_html.write('<title>Landing page</title>')
     dummy_html.write('\n')
@@ -41,22 +43,24 @@ def create_html_index():
     dummy_html.write('\n')
     dummy_html.write('      <h2>Live site:</h2>')
     dummy_html.write('\n')
-    dummy_html.write('        <a href="/web/index.html">Click here to go to the live site</a>')
+    dummy_html.write(
+        '        <a href="/web/index.html">Click here to go to the live site</a>')
     dummy_html.write('\n')
     dummy_html.write('    </div>')
     dummy_html.write('\n')
     dummy_html.write('    <div>')
     dummy_html.write('\n')
-    dummy_html.write('      <h2>Here is the list of the translated pages:</h2>')
+    dummy_html.write(
+        '      <h2>Here is the list of the translated pages:</h2>')
     dummy_html.write('\n')
     dummy_html.write('        <ol>')
-
 
     # Prints all types of txt files present in a Path
     for file in glob.iglob(path, recursive=True):
         printed_file = file.replace('E:\ProyectosOtros\webscraping', '')
         printed_file = printed_file.replace('\\', '/')
-        dummy_html.write('\n           <li><a href="' + printed_file + '">' + printed_file + '</li>')
+        dummy_html.write('\n           <li><a href="' +
+                         printed_file + '">' + printed_file + '</li>')
         print(printed_file)
 
     dummy_html.write('\n')
